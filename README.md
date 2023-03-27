d9fd2fee83dd2c56fd6ec3081c2a37e29073c074
Mustafa El Rouby <48238118+Mustafa-Max@users.noreply.github.com>
2023-03-27 13:25:28 +0200

    al-rouby landing page

---
+++ README.md
@@ -0,0 +1,14 @@
+Header
+
+- add logo
+- list about us, Why us, Contacts us
+
+  ## Sections
+  ## About us
+
+- h2
+- paragraph
+- button
+  background image svg
+  
+## Why us
---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+<header>
+    <nav>
+      <img src="./src/assets/BMax_logistics_office_logo.png" alt="Logo" width="400">
+      <ul>
+        <li>About Us</li>
+        <li>Why Us</li>
+        <li>Contect Us</li>
+      </ul>
+    </nav>
+</header>
+
+<style>
+
+</style>
